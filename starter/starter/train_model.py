0214df9e097c00a9ccac0256600251f3ac2b31a4
Ken Adachi <ke.adachik@gmail.com>
2025-09-21 07:40:01 +0900

    Fix train_model.py: Remove redundant encoder/lb assignment and fix typo in comment

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,11 +1,13 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
+from ml.model import train_model, save_model
 
 # Add the necessary imports for the starter code.
-
+from ml.data import process_data
 # Add code to load in the data.
-
+data = pd.read_csv('data/census.csv')
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
@@ -23,6 +25,10 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
-
+# Process the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
 # Train and save a model.
+model = train_model(X_train, y_train)
+save_model(model, 'model/model.pkl')
